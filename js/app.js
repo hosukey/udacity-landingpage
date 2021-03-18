d81288509d688633ee836084638011e00e69857f
hosu <hosulee11@gmail.com>
2021-03-18 10:28:27 -0700

    add active menu

--- js/app.js
+++ js/app.js
@@ -9,10 +9,8 @@ menu.classList.toggle('active');
 
 
 
-/**
-* Define Global Variables
-* 
-*/
+
+// ScrollInto
 const homeBtn = document.querySelector('.navbar__item--aboutus');
 const homePage = document.querySelector('.section0');
 
@@ -52,37 +50,23 @@ homeBtn.addEventListener('click', ()=>{
 
 
 
-/**
-* End Global Variables
-* Start Helper Functions
-* 
-*/
+// active menu
+const sections = document.querySelectorAll('.section');
+const navLi = document.querySelectorAll ('nav ul li');
 
-
-
-/**
-* End Helper Functions
-* Begin Main Functions
-* 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
-* End Main Functions
-* Begin Events
-* 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+window.addEventListener('scroll', ()=>{
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop-sectionHeight/3)){
+            current = section.getAttribute('id');
+        }
+    })
+navLi.forEach(li =>{
+    li.classList.remove('active');
+    if(li.classList.contains(current)){
+        li.classList.add('active')
+    }
+})
+})
